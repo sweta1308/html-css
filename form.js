41a6f7e02469ad4b4decaec909310fffbe1669a4
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-01-15 18:15:57 +0530

    feat: add function for save and cancel

---
+++ form.js
@@ -0,0 +1,24 @@
+const submitBtn = document.querySelector(".submitBtn");
+const cancelBtn = document.querySelector(".cancelBtn");
+const form = document.querySelector(".form");
+const formElements = form.elements;
+
+const handleSubmit = () => {
+  const userData = {};
+  for (let i = 0; i < formElements.length; i++) {
+    const element = formElements[i];
+    if (element.tagName === "BUTTON") continue; // Exclude the button
+    userData[element.name] = element.value;
+  }
+  alert(
+    `Form Data:\nName: ${userData.name}\nEmail: ${userData.email}\nGender: ${userData.gender}\nHobbies: ${userData.hobbies}\nPlace: ${userData.place}\nDate: ${userData.date}\nBio: ${userData?.bio}`
+  );
+};
+
+const handleCancel = (e) => {
+  e.preventDefault();
+  form.reset();
+};
+
+submitBtn.addEventListener("click", handleSubmit);
+cancelBtn.addEventListener("clicl", handleCancel);
